aa694db5b12f39f365444fdd659fbd22cf7d3f6c
AndreSerafin <andreserafim11@gmail.com>
2022-10-16 22:05:08 -0300

    semana10

--- EstruturasDeDados/Sem10/listaDupla.cpp
+++ EstruturasDeDados/Sem10/listaDupla.cpp
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <conio.h>
 
 struct Carro {
     char nome[80];
@@ -19,24 +21,86 @@ Carro registraCarro();
 void incluirCarroInicio(Node **head, Carro info);
 void incluirCarroFinal(Node **head, Carro info);
 void vizualizaLista(Node *prim);
+void buscaCarro(Node *lista, char nome_do_carro[80]);
+void removeCarro(Node **head, int pos);
 
 main() {
 
     Node *lista = NULL;
-    Carro carro1 = registraCarro();
-    Carro carro2 = registraCarro();
+    
+    int op;
 
-    incluirCarroInicio(&lista, carro1);
-    incluirCarroInicio(&lista, carro1);
-    incluirCarroInicio(&lista, carro1);
-    incluirCarroInicio(&lista, carro1);
+    do{
+        
+        system("cls"); // Comentar se usar terminal integrado
+        printf("1. Incluir um Carro: \n");
+        printf("2. Remover um Carro: \n");
+        printf("3. Listar todos os Carros: \n");
+        printf("4. Pesquisar um Carro pelo nome: \n");
+        printf("5. Sair do Programa: \n");
+        
+        scanf("%d", &op);
 
-    incluirCarroFinal(&lista, carro2);
-    incluirCarroFinal(&lista, carro2);
-    incluirCarroFinal(&lista, carro2);
-    incluirCarroFinal(&lista, carro2);
-    vizualizaLista(lista);
-    system("pause");
+        system("cls"); // Comentar se usar terminal integrado
+
+        switch(op) {
+            case 1:
+                int op2;
+                printf("\n1. Incluir no inicio\n");
+                printf("2. Incluir no final\n");
+                scanf("%d", &op2);
+                switch(op2) {
+                    case 1:
+                        incluirCarroInicio(&lista, registraCarro());
+                        break;
+                    case 2:
+                        incluirCarroFinal(&lista, registraCarro());
+                        break;
+                    default:
+                        printf("Opcao Invalida!\n");
+                        break;
+                }
+                break;
+
+            case 2:
+                if(lista == NULL){
+                    printf("\nLista vazia!\n");
+                    break;
+                }else{
+                    int pos;
+                    vizualizaLista(lista);
+                    printf("Digite a posicao do carro que deseja remover: \n");
+                    scanf("%d", &pos);
+                    removeCarro(&lista, pos);
+                    break;
+                }
+            case 3:
+                if(lista == NULL) {
+                    printf("\nLista vazia!\n");
+                    break;
+                }else{
+                    vizualizaLista(lista);
+                    break;
+                }
+            case 4:
+                if(lista == NULL) {
+                    printf("\nLista vazia!\n");
+                    break;
+                }else{
+                    char nome_carro[80];
+                    printf("Digite o nome do carro: ");
+                    scanf("%s", &nome_carro);
+                    buscaCarro(lista,nome_carro);
+                }
+            case 5:
+                break;
+            default:
+                printf("\nOpcao Invalida!\n");
+        }
+
+        getche();
+
+    }while(op != 5);
     
 
 }
@@ -75,8 +139,11 @@ void incluirCarroFinal(Node **head, Carro info) {
 
 void vizualizaLista(Node *prim) {
     Node *temp = prim;
-    printf("Lista: \n\n");
+    int index = 0;
+    printf("\nLista: \n\n");
     while(temp != NULL) {
+        index++;
+        printf("Carro #%d\n", index);
         printf("Nome do Carro: %s \n", temp->info.nome);
         printf("Ano de Fabricacao: %s \n", temp->info.ano_de_fab);
         printf("Marca: %s \n", temp->info.marca);
@@ -85,6 +152,52 @@ void vizualizaLista(Node *prim) {
     }
 }
 
+void removeCarro(Node** head, int pos)
+{
+    Node* temp;
+    Node* ant;
+    temp = *head;
+    ant = *head;
+    for (int i = 0; i < pos; i++) {
+        if (i == 0 && pos == 1) {
+            *head = (*head)->prox;
+            free(temp);
+        }
+        else {
+            if (i == pos - 1 && temp) {
+                ant->prox = temp->prox;
+                free(temp);
+            }
+            else {
+                ant = temp;
+ 
+                if (ant == NULL)
+                    break;
+                temp = temp->prox;
+            }
+        }
+    }
+}
+ 
+
+void buscaCarro(Node *lista, char nome_do_carro[]) {
+    if(lista == NULL) {
+        printf("Lista Vazia");
+        return;
+    }else {
+        Node *temp = lista;
+        while(temp != NULL) {
+            
+            if(strcmp(temp->info.nome, nome_do_carro) == 0) {
+                printf("\nNome do Carro: %s \n", temp->info.nome);
+                printf("Ano de Fabricacao: %s \n", temp->info.ano_de_fab);
+                printf("Marca: %s \n", temp->info.marca);
+                printf("Preco estimado: R$ %.2f \n\n", temp->info.preco_estimado);
+            }
+            temp = temp->prox;
+        }
+    }
+}
 
 Carro registraCarro() {
     Carro novoCarro;
